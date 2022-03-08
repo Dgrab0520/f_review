4e80536de39637313f1b99363cd5c583876adac1
TodaudRur <skstjdtndi01@naver.com>
2022-03-08 17:15:05 +0900

    0308 big changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-import 'main_page/main_page.dart';
+import 'ui/main_page/main_page.dart';
 
 void main() {
   runApp(const MyApp());
